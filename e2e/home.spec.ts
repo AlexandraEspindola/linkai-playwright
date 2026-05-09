799cf881d4c79a2d5ef3fe48a623c1c81265a44c
Alexandra Espindola <ale.espindola.silva@gmail.com>
2026-05-09 19:27:30 -0300

    Update home test and remove unused spec

--- e2e/home.spec.ts
+++ e2e/home.spec.ts
@@ -9,14 +9,4 @@ test("deve exibir o titulo", async ({ page }) => {
   await page.waitForTimeout(3000);
 });
 
-// test("get started link", async ({ page }) => {
-//   await page.goto("https://playwright.dev/");
 
-//   // Click the get started link.
-//   await page.getByRole("link", { name: "Get started" }).click();
-
-//   // Expects page to have a heading with the name of Installation.
-//   await expect(
-//     page.getByRole("heading", { name: "Installation" }),
-//   ).toBeVisible();
-// });
--- e2e/scheduleapt.spec.ts
+++
@@ -1,8 +0,0 @@
-// import { test, expect } from "@playwright/test";
-
-// test("deve abrir o formulario Schedule an appointment ", async ({ page }) => {
-//   await page.goto(
-//     "https://www.harrywinston.com/en/services/schedule-an-appointment",
-//   );
-//   await page.waitForTimeout(3000);
-// });
